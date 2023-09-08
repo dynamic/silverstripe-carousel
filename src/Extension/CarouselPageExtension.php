fcca7a2f229627d931a6a8f25f7b1ecd570257ed
Jason Irish <jirish@dynamicagency.com>
2023-09-07 22:50:42 -0500

    FEATURE Bootstrap 5 updates

--- src/Extension/CarouselPageExtension.php
+++ src/Extension/CarouselPageExtension.php
@@ -3,21 +3,20 @@
 namespace Dynamic\Carousel\Extension;
 
 use SilverStripe\View\SSViewer;
-use SilverStripe\Core\ClassInfo;
 use Dynamic\Carousel\Model\Slide;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
 use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 use Symbiote\GridFieldExtensions\GridFieldAddNewMultiClass;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
 use Symbiote\GridFieldExtensions\GridFieldAddExistingSearchButton;
 
 /**
  * Class \Dynamic\Carousel\Extension\CarouselPageExtension
  *
- * @property HomePage|CarouselPageExtension $owner
+ * @property BlockPage|CampaignLandingPage|HomePage|CarouselPageExtension $owner
  * @method ManyManyList|Slide[] Slides()
  */
 class CarouselPageExtension extends DataExtension
@@ -51,14 +50,17 @@ class CarouselPageExtension extends DataExtension
             GridFieldConfig_RelationEditor::create()
         );
         $grid->getConfig()
-            ->removeComponentsByType(GridFieldAddNewButton::class)
+            ->removeComponentsByType([
+                GridFieldAddNewButton::class,
+                GridFieldAddExistingAutocompleter::class,
+            ])
             ->addComponents([
                 $multiClass = GridFieldAddNewMultiClass::create(),
                 new GridFieldOrderableRows('SortOrder'),
                 new GridFieldAddExistingSearchButton(),
             ]);
 
-        $fields->addFieldToTab('Root.Slides', $grid);
+        $fields->addFieldToTab('Root.Hero', $grid);
     }
 
     /**
---
+++ templates/Includes/ImageSlide.ss
@@ -0,0 +1,5 @@
+<img src="$Image.Fill(1200,500).URL" class="d-block w-100 image-fluid" alt="$Image.Title.XML">
+<div class="carousel-caption d-none d-md-block">
+    <h3>$Title</h3>
+    $Content
+</div>
