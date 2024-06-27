51fd3428b294ca24da28996ee1e82bd7d7df2edd
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2024-06-27 09:46:42 -0500

    UPDATE utilize embed/embed to allow for embedding video

--- src/Model/VideoSlide.php
+++ src/Model/VideoSlide.php
@@ -2,8 +2,10 @@
 
 namespace Dynamic\Carousel\Model;
 
+use Embed\Embed;
 use SilverStripe\Assets\File;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\FieldType\DBField;
 
 /**
  * Class \Dynamic\Carousel\Model\VideoSlide
@@ -35,7 +37,8 @@ class VideoSlide extends Slide
      */
     private static $db = [
         'VideoType' => 'Enum(["Embed","Native"], "Embed")',
-        'VideoEmbed' => 'Text',
+        'SourceURL' => 'Varchar(255)',
+        'iFrameCSS' => 'Varchar',
     ];
 
     /**
@@ -53,6 +56,13 @@ class VideoSlide extends Slide
     ];
 
     /**
+     * @var array|string[]
+     */
+    private static array $allowed_embed_types = [
+        'video',
+    ];
+
+    /**
      * @return FieldList
      */
     public function getCMSFields(): FieldList
@@ -61,10 +71,96 @@ class VideoSlide extends Slide
             $fields->dataFieldByName('Video')
                 ->displayIf('VideoType')->isEqualTo('Native');
 
-            $fields->dataFieldByName('VideoEmbed')
+            $fields->dataFieldByName('SourceURL')
                 ->displayIf('VideoType')->isEqualTo('Embed');
+
+            $fields->dataFieldByName('iFrameCSS')
+                ->displayIf('VideoType')->isEqualTo('Embed')
+                ->setDescription('Add CSS classes to the iFrame');
         });
 
+
         return parent::getCMSFields();
+    }
+
+    public function getVideoEmbed()
+    {
+        if ($this->SourceURL) {
+            $embed = new Embed();
+            $info = $embed->get($this->SourceURL);
+
+            $source = $this->parseEmbedSource($info->code->html);
+            $parameters = $this->getEmbedParameters($info->providerName);
+
+            $components = parse_url($source);
+            parse_str($components['query'], $queryParams);
+            $queryParams = array_merge($queryParams, $parameters);
+
+            $components['query'] = http_build_query($queryParams);
+            $newUrl = $components['scheme'] . '://' . $components['host'] . $components['path'] . '?' . $components['query'];
+
+            $newIframe = str_replace($source, htmlspecialchars($newUrl), $info->code->html);
+            $newIframe = preg_replace('/(width|height)="\d*"\s/', '', $newIframe);
+
+            if ($this->iFrameCSS) {
+                $newIframe = str_replace('<iframe ', '<iframe class="' . $this->iFrameCSS . '" ', $newIframe);
+            }
+
+
+            return DBField::create_field('HTMLText', $newIframe);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $provider
+     * @return string[]
+     */
+    protected function getEmbedParameters($provider): array
+    {
+        switch ($provider) {
+            case 'Vimeo':
+                $parameters = [
+                    'autoplay' => '1',
+                    'background' => '1', // Vimeo uses 'background' parameter for background videos
+                    'loop' => '1',
+                    'muted' => '1', // 'muted' for Vimeo instead of 'mute'
+                    'playsinline' => '1',
+                    'title' => '0',
+                    'byline' => '0',
+                    'portrait' => '0',
+                ];
+                break;
+            case 'YouTube':
+                $parameters = [
+                    'autoplay' => '1',
+                    'mute' => '1', // Necessary for autoplay in most browsers
+                    'loop' => '1',
+                    'playsinline' => '1',
+                    'controls' => '0',
+                    'frameborder' => '0',
+                ];
+                break;
+            default:
+                $parameters = [
+                    'autoplay' => '1',
+                    'mute' => '1', // Necessary for autoplay in most browsers
+                ];
+                break;
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @param $code
+     * @return string
+     */
+    protected function parseEmbedSource($code): string
+    {
+        preg_match('/src="([^"]+)"/', $code, $source);
+
+        return html_entity_decode($source[1]);
     }
 }
--- templates/Includes/VideoSlide.ss
+++ templates/Includes/VideoSlide.ss
@@ -1,46 +1,7 @@
 <div class="embed-responsive embed-responsive-16by9">
-    <% if $VideoType == Embed %>
-        $VideoEmbed.Raw
-    <% else_if $VideoType == Native %>
+    <% if $VideoType == 'Embed' %>
+        $VideoEmbed
+    <% else_if $VideoType == 'Native' %>
         $Video
     <% end_if %>
 </div>
-<script>
-    var player;
-
-    window.onYouTubeIframeAPIReady = function() {
-        var iframe = document.querySelector('#carousel-1 .embed-responsive-item');
-        player = new YT.Player(iframe, {
-            events: {
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
-    function onPlayerStateChange(event) {
-        var carouselElement = document.getElementById('carousel-1');
-        var carouselInstance = new bootstrap.Carousel(carouselElement);
-
-        if (event.data == YT.PlayerState.PLAYING) {
-            carouselInstance.pause();
-        } else if (event.data == YT.PlayerState.ENDED) {
-            carouselInstance.cycle();
-        }
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-        var carouselElement = document.getElementById('carousel-1');
-        carouselElement.addEventListener('slid.bs.carousel', function(e) {
-            if (e.relatedTarget.querySelector('iframe') && player.getPlayerState() !== YT.PlayerState.PLAYING) {
-                player.playVideo();
-            }
-        });
-    });
-</script>
-
-
