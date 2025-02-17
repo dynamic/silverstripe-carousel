be671a77203137dbd3ff837cbcd1b18c7ad50aff
Jason Irish <jirish@dynamicagency.com>
2025-02-17 16:24:14 -0600

    REFACTOR Augment Slide models
    VIdeo Slide:
    * add EmbedObject via EmbedField
    * choose between native and embed
    
    Image Slide:
    * Mobile image upload field
    
    Slide:
    * ShowCaption method

--- src/Model/VideoSlide.php
+++ src/Model/VideoSlide.php
@@ -4,6 +4,10 @@ namespace Dynamic\Carousel\Model;
 
 use SilverStripe\Assets\File;
 use SilverStripe\Forms\FieldList;
+use nathancox\EmbedField\Forms\EmbedField;
+use nathancox\EmbedField\Model\EmbedObject;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
+use SilverStripe\AssetAdmin\Forms\UploadField;
 
 /**
  * Class \Dynamic\Carousel\Model\VideoSlide
@@ -41,6 +45,7 @@ class VideoSlide extends Slide
      */
     private static $has_one = [
         'Video' => File::class,
+        'EmbedVideo' => EmbedObject::class,
     ];
 
     /**
@@ -56,7 +61,33 @@ class VideoSlide extends Slide
     public function getCMSFields(): FieldList
     {
         $this->beforeUpdateCMSFields(function (FieldList $fields) {
-            //$fields->
+            // Remvove fields
+            $fields->removeByName([
+                'EmbedVideoID',
+                'Video',
+            ]);
+
+            // Native video
+            $uploadVideo = UploadField::create('Video', 'Upload video')
+            ->setFolderName('Uploads/Carousel/Videos');
+
+            $fields->insertAfter(
+                'VideoType',
+                $uploadVideo
+            );
+
+            // Embed video
+            // @phpstan-ignore class.notFound
+            $embedVideo = Wrapper::create(EmbedField::create('EmbedVideoID', 'Embed video'));
+
+            $fields->insertAfter(
+                'VideoType',
+                $embedVideo
+            );
+
+            //
+            $uploadVideo->displayIf("VideoType")->isEqualTo("Native");
+            $embedVideo->displayIf("VideoType")->isEqualTo("Embed");
         });
 
         return parent::getCMSFields();
